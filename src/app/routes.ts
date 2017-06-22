7973eda6d7528c1d9976738f0905756b6594a467
Adam <adam.bojarczuk@motofocus.pl>
2017-06-22 20:22:05 +0200

    Commit zdalny

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,16 +1,16 @@
-import {Component} from "@angular/core";
-import {Routes} from "@angular/router";
+import { Component } from "@angular/core";
+import { Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
+	{
+		path: '',
+		component: DashboardComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	}
 ];
